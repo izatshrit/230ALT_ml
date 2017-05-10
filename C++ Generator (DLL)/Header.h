d473fdaab5fa91376194575a63bb1d2b4474c8fb
AhmadAlkhatib <n.ahmadalkhatib@gmail.com>
2017-05-10 10:56:22 +0300

    delete DLLS

--- C++ Generator (DLL)/Header.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-using namespace std;
-#define myGen __declspec(dllexport)
-
-extern "C" {
-	myGen void generator();
-}
--- Final Sorter (DLL)/Header.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-using namespace std;
-#define myIndexer __declspec(dllexport)
-
-extern "C" {
-	myIndexer void filter(const char* ptr);
-}
